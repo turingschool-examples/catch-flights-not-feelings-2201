8ccc558c7103aa3a607c4f2257762d8a5864ab36
Jessica Grazulis <jlgrazulis@gmail.com>
2022-03-07 12:03:53 -0700

    Remove pry

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,7 +5,6 @@ class FlightsController < ApplicationController
   end
 
   def destroy
-    require 'pry'; binding.pry
     # @flight = Flight.find(params{:id})
     @flight.passengers.destroy
   end
