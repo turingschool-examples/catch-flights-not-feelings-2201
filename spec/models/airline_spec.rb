033f94174fa7cc5b3069a60a4c559b78cb80d816
Richard LaBrecque <richardlabreque@gmail.com>
2022-03-07 13:25:43 -0500

    test: add relationship test for airline to passengers

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Airline, type: :model do
   it { should have_many :flights }
+  it {should have_many(:passengers).through(:flights)}
 
   describe 'instance methods' do
     before(:each) do
