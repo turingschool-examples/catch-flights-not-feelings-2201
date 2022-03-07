b0fa607597d799050a94f46488d85f9160551dad
Paul Leonard <paulpaulleonard@gmail.com>
2022-03-07 13:23:23 -0500

    Instead of destroying through flight update, I moved it to the correct controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,12 +3,4 @@ class FlightsController < ApplicationController
   def index
     @flights = Flight.all
   end
-
-  def update
-    flight = Flight.find(params[:flight_id])
-    passenger = Passenger.find(params[:id])
-    flight_passenger = FlightPassenger.find_by(flight_id: flight.id, passenger_id: passenger.id)
-    flight_passenger.destroy
-    redirect_to '/flights'
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get '/flights', to: 'flights#index'
 
-  patch '/flights/:flight_id/:id', to: 'flights#update'
+  delete '/flights/:flight_id/:id/destroy', to: 'flight_passengers#destroy'
 
   get '/airlines/:id', to: 'airlines#show'
 end
