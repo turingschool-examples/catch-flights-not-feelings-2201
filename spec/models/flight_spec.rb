8b15ac89157b39585554d1330f08781cd3f7f31e
Aedan Yturralde <aedanyturralde@gmail.com>
2022-03-07 09:15:28 -0700

    test: write tests for model relationships

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Flight, type: :model do
-  it { should belong_to :airline }
+RSpec.describe Flight, type: :model do 
+  describe 'relationships' do 
+    it { should belong_to :airline }
+    it {should have_many :flight_passengers}
+    it {should have_many(:passengers).through(:flight_passengers)}
+  end 
 end
