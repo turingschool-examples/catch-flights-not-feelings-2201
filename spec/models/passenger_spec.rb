80bf63257efa091e1c2d4d6901fa75697b828db5
TJ Williams <williamsjr.timothy1@gmail.com>
2022-03-07 13:06:13 -0500

    Test/Feat: adult method returns adult passengers(>= 18) once

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -3,4 +3,19 @@ require 'rails_helper'
 RSpec.describe Passenger, type: :model do
   it { should have_many :flight_passengers }
   it { should have_many(:flights).through(:flight_passengers) }
+
+
+  it "should return adult passengers(older than or equal to 18) once" do
+
+    passenger = Passenger.create!(name: "Zerg", age: 32)
+    passenger2 = Passenger.create!(name: "Tina", age: 34)
+    passenger3 = Passenger.create!(name: "Little Tina", age: 9)
+
+
+    passenger4 = Passenger.create!(name: "Terran", age: 32)
+    passenger5 = Passenger.create!(name: "Bob", age: 36)
+    passenger6 = Passenger.create!(name: "Little Bob", age: 8)
+
+    expect(Passenger.adult).to eq([passenger,passenger2,passenger4,passenger5])
+  end
 end
