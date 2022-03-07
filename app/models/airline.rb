25c2014ef1f101fcf00759697bb106000ffa1df7
Austin Moore <austinchristianmoore@gmail.com>
2022-03-07 13:26:39 -0500

    Feature: Complete US3

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,9 @@
 class Airline < ApplicationRecord
   has_many :flights
+  has_many :passenger_flights, through: :flights
+  has_many :passengers, through: :passenger_flights
+
+  def list_all_adult_passengers
+    self.passengers.where('age >= 18').distinct
+  end
 end
--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Airline, type: :model do
   it { should have_many :flights }
+  it { should have_many(:passenger_flights).through :flights }
+  it { should have_many(:passengers).through :passenger_flights }
 
   describe "instance methods" do
     it ".list_all_adult_passengers" do
