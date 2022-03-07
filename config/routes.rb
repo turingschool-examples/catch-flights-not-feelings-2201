0d9be85a72e4ef540cdd7a6723e55b4972edd5a4
Tamara Dowis <tamara.dowis@gmail.com>
2022-03-07 11:10:39 -0600

    -madd seed data

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+    resources :airlines, only: [:index]
+    resources :flights, only: [:show]
+    resources :passengers, only: [:show]
+  end
---
+++ spec/features/index_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Flight Index Page', type: :feature do
+
+  describe 'index page will display attributes' do
+
+   it 'will display flight numbers with the airline name and coorsponding passengers' do
+    visit
+
+   expect(page).to have_content()
+
+   expect(page).to_not have_content()
+
+   expect(page).to have_link()
+
+   expect(current_path).to eq()
+
+   end
+  end
+end
