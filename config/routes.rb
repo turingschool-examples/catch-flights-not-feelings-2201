5c21b489b7702934df26fa0f6dd8b157e38969c8
Austin Moore <austinchristianmoore@gmail.com>
2022-03-07 12:59:43 -0500

    Feature: US3 render and rounting

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resources :flights, only: [:index] do
     resources :passengers, only: [:destroy], controller: 'passenger_flights'
   end
+
+  resources :airlines, only: [:show]
+
 end
