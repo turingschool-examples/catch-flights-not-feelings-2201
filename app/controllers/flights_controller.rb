bf099309373bce4797f1e850e948d3bd106a0d5b
Jessica Grazulis <jlgrazulis@gmail.com>
2022-03-07 16:30:43 -0700

    Add destroy method to passenger_flights controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,11 +1,5 @@
 class FlightsController < ApplicationController
   def index
-    # @flights = Flight.all
     @airlines = Airline.all
-  end
-
-  def destroy
-    # @flight = Flight.find(params{:id})
-    @flight.passengers.destroy
   end
 end
