b95fe70aba6bbfb301c2719cd6437c06aa9605c5
Aedan Yturralde <aedanyturralde@gmail.com>
2022-03-07 10:56:47 -0700

    test: write test for passenger class method return_adults. Define method in model pass test

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe Passenger, type: :model do
     it {should have_many :flight_passengers}
     it {should have_many(:flights).through(:flight_passengers)}
   end 
+
+  describe 'class methods' do 
+    describe '#return_adults' do 
+      it 'returns all passengers that are older than 17' do 
+        FlightPassenger.destroy_all
+        Passenger.destroy_all
+        passenger1 = Passenger.create!(name: "Tina", age: 15)
+        passenger2 = Passenger.create!(name: "Linda", age: 43)
+        passenger3 = Passenger.create!(name: "Louise", age: 12)
+        passenger4 = Passenger.create!(name: "Bob", age: 44)
+
+        expect(Passenger.return_adults).to eq([passenger2, passenger4])
+      end 
+    end
+    
+  end 
 end
