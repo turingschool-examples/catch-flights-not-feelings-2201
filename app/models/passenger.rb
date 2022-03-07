fc00968c1cc3e952c5463a7d2cb7c5b5e56e7acf
Joseph W Galvin <jwgalvin@gmail.com>
2022-03-07 11:25:52 -0700

    passenger model validations and tests added

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,4 +2,6 @@ class Passenger < ApplicationRecord
   has_many :passenger_flights, dependent: :destroy
   has_many :flights, through: :passenger_flights
 
+  validates_presence_of :name
+  validates_presence_of :age
 end
--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Passenger, type: :model do
-
+  it { should have_many :flights }
+  it { should have_many :passenger_flights }
+  it { should validate_presence_of :name}
+  it { should validate_presence_of :age}
 end
