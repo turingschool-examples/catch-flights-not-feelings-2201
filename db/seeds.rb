18c661962b6cd2c4f41d84300534c712421bf504
Sam Iavri <sammi80134@gmail.com>
2022-03-07 09:26:47 -0700

    data set up

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+american = Airline.create!(name: 'American')
+delta = Airline.create!(name: 'Delta')
+jet_blue = Airline.create!(name: 'Jet Blue')
+southwest = Airline.create!(name: 'Southwest')
+united = Airline.create!(name: 'United')
+
+flight1 = american.flights.create!(number: '7990', date: '2/7/2022', departure_city: 'Glendale', arrival_city: 'Dallas')
+flight2 = american.flights.create!(number: '3940', date: '3/10/2022', departure_city: 'Detroit',
+                                   arrival_city: 'Orlando')
+flight3 = delta.flights.create!(number: '9091', date: '5/17/2022', departure_city: 'Chicago',
+                                arrival_city: 'Kansas City')
+flight4 = southwest.flights.create!(number: '6798', date: '10/9/2022', departure_city: 'Tulsa',
+                                    arrival_city: 'Fresno')
+flight5 = united.flights.create!(number: '9665', date: '11/22/2022', departure_city: 'Durham',
+                                 arrival_city: 'New Orleans')
+tina = Passenger.create!(name: 'Tina', age: 13)
+louise = Passenger.create!(name: 'Louise', age: 9)
+bob = Passenger.create!(name: 'Bob', age: 46)
+linda = Passenger.create!(name: 'Linda', age: 44)
+big_bob = Passenger.create!(name: 'Big Bob', age: 84)
