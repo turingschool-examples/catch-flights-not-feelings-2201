023fd8a36f58678f899b163b3bb090e25366095e
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-03-07 13:26:26 -0500

    Feat: create model method for extension

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -3,7 +3,10 @@ class Airline < ApplicationRecord
   has_many :passenger_flights, through: :flights
   has_many :passengers, through: :passenger_flights
 
-  def adult_passengers
-    passengers.where('age > 18').distinct
+  def adult_passengers_by_frequent
+    passengers.joins(:passenger_flights)
+              .select("passengers.name, COUNT(passenger_flights.*) AS total_flights")
+              .group('passengers.name').where('passengers.age >= 18')
+              .order('total_flights DESC')
   end
 end
