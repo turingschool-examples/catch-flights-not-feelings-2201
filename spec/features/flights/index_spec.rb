8696dd002dd7be261c3c6a49974b114d123365ba
Ian Ross <ianrossdrums@gmail.com>
2022-03-07 11:43:27 -0700

    feat: us1 tests pass

--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'flight index page' do
   before(:each) do
-    @airline1 = Airline.create(name: "JetBlue")
-    @airline2 = Airline.create(name: "Southwest")
-    @airline3 = Airline.create(name: "United")
+    @airline1 = Airline.create!(name: "JetBlue")
+    @airline2 = Airline.create!(name: "Southwest")
+    @airline3 = Airline.create!(name: "United")
 
-    @flight1 = Flight.create(number: "7990", date: "2/7/2022", departure_city: "Glendale", arrival_city: "Dallas", airline_id: 1)
-    @flight2 = Flight.create(number: "3940", date: "3/10/2022", departure_city: "Detroit", arrival_city: "Orlando", airline_id: 1)
-    @flight3 = Flight.create(number: "9091", date: "5/17/2022", departure_city: "Chicago", arrival_city: "Kansas City", airline_id: 2)
-    @flight4 = Flight.create(number: "6798", date: "10/9/2022", departure_city: "Tulsa", arrival_city: "Fresno", airline_id: 3)
-    @flight5 = Flight.create(number: "9665", date: "11/22/2022", departure_city: "Durham", arrival_city: "New Orleans", airline_id: 3)
+    @flight1 = Flight.create!(number: "7990", date: "2/7/2022", departure_city: "Glendale", arrival_city: "Dallas", airline_id: @airline1.id)
+    @flight2 = Flight.create!(number: "3940", date: "3/10/2022", departure_city: "Detroit", arrival_city: "Orlando", airline_id: @airline1.id)
+    @flight3 = Flight.create!(number: "9091", date: "5/17/2022", departure_city: "Chicago", arrival_city: "Kansas City", airline_id: @airline2.id)
+    @flight4 = Flight.create!(number: "6798", date: "10/9/2022", departure_city: "Tulsa", arrival_city: "Fresno", airline_id: @airline3.id)
+    @flight5 = Flight.create!(number: "9665", date: "11/22/2022", departure_city: "Durham", arrival_city: "New Orleans", airline_id: @airline3.id)
 
     @passenger1  = Passenger.create(name: "Pabu", age: 20)
     @passenger2  = Passenger.create(name: "Thor", age: 15)
