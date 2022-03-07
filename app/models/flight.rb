617b3b1b713ae9204f305c0bb3c5b484ed611cc4
Jessica Grazulis <jlgrazulis@gmail.com>
2022-03-07 12:04:40 -0700

    Add model method

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,14 @@ class Flight < ApplicationRecord
   belongs_to :airline
   has_many :passenger_flights
   has_many :passengers, through: :passenger_flights
+
+  def adult_passengers
+    adult_passengers = []
+    self.passengers.each do |passenger| 
+      if passenger.age >= 18
+        adult_passengers << passenger
+      end 
+    end
+    adult_passengers
+  end
 end
