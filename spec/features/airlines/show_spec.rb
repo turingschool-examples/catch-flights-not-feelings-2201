96e3201424870389c401f3692649de0eb10368e4
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-07 11:11:24 -0700

    Test: Update test to account for sad paths.

--- spec/features/airlines/show_spec.rb
+++ spec/features/airlines/show_spec.rb
@@ -11,11 +11,13 @@ describe 'airline show page' do
     @sakic = Passenger.create!(name: 'Sakic', age: 27)
     @onyx = Passenger.create!(name: 'Onyx', age: 29)
     @priska = Passenger.create!(name: 'Priska', age: 27)
+    @lynn = Passenger.create!(name: 'Lynn', age: 17)
 
     @flight_pass_1 = FlightPassenger.create!(flight_id: @flight_1.id, passenger_id: @gunnar.id)
     @flight_pass_2 = FlightPassenger.create!(flight_id: @flight_1.id, passenger_id: @sakic.id)
     @flight_pass_3 = FlightPassenger.create!(flight_id: @flight_2.id, passenger_id: @onyx.id)
     @flight_pass_4 = FlightPassenger.create!(flight_id: @flight_2.id, passenger_id: @priska.id)
+    @flight_pass_5 = FlightPassenger.create!(flight_id: @flight_1.id, passenger_id: @lynn.id)
 
 
     visit airline_path(@luftansa.id)
@@ -28,5 +30,8 @@ describe 'airline show page' do
     expect(page).to have_content("Passengers: #{@gunnar.name} #{@sakic.name}")
     expect(page).to_not have_content(@onyx.name)
     expect(page).to_not have_content(@priska.name)
+  end
+  it 'I do not see under 18 passengers that have flights on that airline' do
+    expect(page).to_not have_content(@lynn.name)
   end
 end
