2b24e6f2a25c0904e879a03dcb9d6285b18dc17f
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-07 11:38:39 -0700

    US3 Complete, Features/Models coverage 100%

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :flights, only: :index
+  resources :airlines, only: :show
   delete '/flights/:id/passengers', to: 'flight_passengers#destroy'
 end
