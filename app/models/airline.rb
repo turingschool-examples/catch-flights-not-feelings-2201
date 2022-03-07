38ae63e511fc76da713a68db87e23d43384aa700
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-03-07 12:52:48 -0500

    Feat: add instance method adult passenger in airline model

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -2,4 +2,8 @@ class Airline < ApplicationRecord
   has_many :flights
   has_many :passenger_flights, through: :flights
   has_many :passengers, through: :passenger_flights
+
+  def adult_passengers
+    passengers.where('age > 18').distinct
+  end
 end
