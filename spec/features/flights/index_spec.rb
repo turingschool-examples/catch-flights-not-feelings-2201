9032173f2a41f3108efe7192b3fa8aaaf9f21c00
Paul Leonard <paulpaulleonard@gmail.com>
2022-03-07 11:49:20 -0500

    user story one 1/3 done

---
+++ spec/features/flights/index_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "Flights Index Page" do
+  describe 'When I visit the Flights index page,' do
+    it "I see a list of all flight numbers" do
+      airline = Airline.create!(name: "American")
+      flight1 = airline.flights.create!(number: "7990", date: "2/7/2022", departure_city: "Glendale", arrival_city: "Dallas")
+      flight2 = airline.flights.create!(number: "3940", date: "3/10/2022", departure_city: "Detroit", arrival_city: "Orlando")
+      flight3 = airline.flights.create!(number: "9091", date: "5/17/2022", departure_city: "Chicago", arrival_city: "Kansas City")
+      flight4 = airline.flights.create!(number: "6798", date: "10/9/2022", departure_city: "Tulsa", arrival_city: "Fresno")
+      flight5 = airline.flights.create!(number: "9665", date: "11/22/2022", departure_city: "Durham", arrival_city: "New Orleans")
+
+      visit '/flights'
+
+      expect(page).to have_content(flight1.number)
+      expect(page).to have_content(flight2.number)
+      expect(page).to have_content(flight3.number)
+      expect(page).to have_content(flight4.number)
+      expect(page).to have_content(flight5.number)
+    end
+
+    it "next to each flight number, I see the name of the airline of the flight" do
+
+    end
+
+    it "under each flight number, I see the names of all that flights passengers" do
+
+    end
+  end
+end
