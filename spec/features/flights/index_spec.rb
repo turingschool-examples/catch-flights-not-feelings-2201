1549604e8e69c6ed318cd060c18cd6ace83f6dbd
Becky Nisttahuz <benistta@hotmail.com>
2022-03-07 09:49:48 -0700

    Test: Create test for user story 1

---
+++ spec/features/flights/index_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+# User Story 1, Flights Index Page
+#
+# As a visitor
+# When I visit the flights index page
+# I see a list of all flight numbers
+# And next to each flight number I see the name of the Airline of that flight
+# And under each flight number I see the names of all that flight's passengers
+
+RSpec.describe 'flight index page' do
+  it 'can see the name of the Airline of that flight' do
+    airline = Airline.create(name: "American")
+    airline_2 = Airline.create(name: "LAM")
+    flight_1 = airline.flights.create(number: "1727", date: "08/03/20", departure_city: "Denver", arrival_city: "Reno")
+    flight_2 = airline.flights.create(number: "1567", date: "07/03/20", departure_city: "Cali", arrival_city: "Bogota")
+    flight_3 = airline.flights.create(number: "4777", date: "06/03/20", departure_city: "La Paz", arrival_city: "Santa Cruz")
+
+
+    ana = Passenger.create(name: 'Ana', age: 45)
+    eva = Passenger.create(name: 'Eva', age: 37)
+    nelson = Passenger.create(name: 'Nelson', age: 40)
+    adalid = Passenger.create(name: 'Adalid', age: 4)
+
+    PassengerFlight.create(flight_id: flight_1.id, passenger_id: ana.id)
+    PassengerFlight.create(flight_id: flight_1.id, passenger_id: eva.id)
+    PassengerFlight.create(flight_id: flight_2.id, passenger_id: nelson.id)
+    PassengerFlight.create(flight_id: flight_3.id, passenger_id: adalid.id)
+
+    visit "/flights"
+
+    within "#flight-#{flight_1.id}" do
+      expect(page).to have_content(flight_1.number)
+      expect(page).to have_content(airline.number)
+      expect(page).to have_content(ana.name)
+      expect(page).to have_content(eva.name)
+    end
+
+    within "#flight-#{flight_2.id}" do
+      expect(page).to have_content(flight_2.number)
+      expect(page).to have_content(airline.number)
+      expect(page).to have_content(nelson.name)
+    end
+
+    within "#flight-#{flight_3.id}" do
+      expect(page).to have_content(flight_3.number)
+      expect(page).to have_content(airline.number)
+      expect(page).to have_content(adalid.name)
+    end
+  end
+end
