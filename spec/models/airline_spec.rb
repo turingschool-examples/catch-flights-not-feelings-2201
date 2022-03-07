f6595cf39dcf02e9497d4cd1c6bc1e656db90f12
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-03-07 13:25:44 -0500

    Test: create test for instance method in model

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -11,25 +11,31 @@ RSpec.describe Airline, type: :model do
 
       @flight1 = @jetblue.flights.create!(number: "1", date: "01/01/2022", departure_city: "Miami", arrival_city: "Dallas", airline_id: @jetblue.id)
       @flight2 = @jetblue.flights.create!(number: "2", date: "01/10/2022", departure_city: "New Orleans", arrival_city: "Los Angeles", airline_id: @jetblue.id)
+      @flight3 = @jetblue.flights.create!(number: "3", date: "01/20/2022", departure_city: "Los Angeles", arrival_city: "London", airline_id: @jetblue.id)
 
       @tupac = Passenger.create!(name: "Tupac", age: 24)
       @biggie = Passenger.create!(name: "Biggie", age: 17)
       @wayne = Passenger.create!(name: "Wayne", age: 21)
+      @nipsey = Passenger.create!(name: "Nipsey", age: 21)
 
       @fp1 = PassengerFlight.create!(flight_id: @flight1.id, passenger_id: @tupac.id)
       @fp2 = PassengerFlight.create!(flight_id: @flight1.id, passenger_id: @wayne.id)
       @fp3 = PassengerFlight.create!(flight_id: @flight2.id, passenger_id: @wayne.id)
       @fp4 = PassengerFlight.create!(flight_id: @flight2.id, passenger_id: @biggie.id)
+      @fp5 = PassengerFlight.create!(flight_id: @flight3.id, passenger_id: @tupac.id)
+      @fp6 = PassengerFlight.create!(flight_id: @flight3.id, passenger_id: @nipsey.id)
+      @fp7 = PassengerFlight.create!(flight_id: @flight3.id, passenger_id: @wayne.id)
     end
 
-    describe '.adult_passengers' do
-      it 'provides a list of unique adult passengers' do
-        result = @jetblue.adult_passengers
+    describe '.adult_passengers_by_frequent' do
+      it 'provides a list of unique adult passengers, ordered by most frequent' do
+        result = @jetblue.adult_passengers_by_frequent
 
-        expect(result).to include(@tupac)
-        expect(result).to include(@wayne)
-        expect(result).to_not include(@biggie)
-        expect(result.length).to eq(2)
+        expect(result[0].name).to eq(@wayne.name)
+        expect(result[1].name).to eq(@tupac.name)
+        expect(result[2].name).to eq(@nipsey.name)
+        expect(result).to_not include(@biggie.name)
+        expect(result.length).to eq(3)
       end
     end
   end
