8efad333d6550107d02641f90f4fd2f0b9c5bab2
Joseph W Galvin <jwgalvin@gmail.com>
2022-03-07 11:43:48 -0700

    Last model fix.  validations added to flights and tested

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,9 @@ class Flight < ApplicationRecord
   belongs_to :airline
   has_many :passenger_flights, dependent: :destroy
   has_many :passengers, through: :passenger_flights
+
+  validates_presence_of :number
+  validates_presence_of :date
+  validates_presence_of :departure_city
+  validates_presence_of :arrival_city
 end
