3e30163e92d3ba3cc66a0359e36572c40747670f
Oscar Santos <oscar303data@gmail.com>
2022-03-07 11:20:55 -0700

    Finish user story 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/flights', to: 'flights#index'
+  delete '/flights/:id/passengers', to: 'passenger_flights#destroy'
 end
--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -3,14 +3,14 @@ RSpec.describe 'flights Index Page' do
 it  'displays flights attributes' do
 
     frontier = Airline.create!(name: 'Frontier')
-    flight_1 = frontier.flights.create!(number: '1', date: '1/1/22', departure_city: 'Denver', arrival_city: 'DC')
+    flight_1 = frontier.flights.create!(number: '2563', date: '01/08/22', departure_city: 'Miami', arrival_city: 'Las Vegas')
     passenger_1 = Passenger.create!(name: 'Josh', age: 29)
     passenger_2 = Passenger.create!(name: 'Peter', age: 27)
     PassengerFlight.create!(flight_id: flight_1.id, passenger_id: passenger_1.id)
     PassengerFlight.create!(flight_id: flight_1.id, passenger_id: passenger_2.id)
 
     delta = Airline.create!(name: 'Delta')
-    flight_2 = delta.flights.create!(number: '2', date: '1/2/22', departure_city: 'Denver', arrival_city: 'DC')
+    flight_2 = delta.flights.create!(number: '2784', date: '01/11/21', departure_city: 'Hawai', arrival_city: 'New York')
     passenger_3 = Passenger.create!(name: 'Luis', age: 29)
     passenger_4 = Passenger.create!(name: 'Miguel', age: 27)
     PassengerFlight.create!(flight_id: flight_2.id, passenger_id: passenger_3.id)
@@ -24,4 +24,27 @@ it  'displays flights attributes' do
     expect(page).to have_content("Passengers: #{passenger_3.name} #{passenger_4.name}")
 
 end
+
+it 'can remove a passenger' do
+  frontier = Airline.create!(name: 'Frontier')
+  flight_1 = frontier.flights.create!(number: '2563', date: '01/08/22', departure_city: 'Miami', arrival_city: 'Las Vegas')
+  passenger_1 = Passenger.create!(name: 'Josh', age: 19)
+  passenger_2 = Passenger.create!(name: 'Peter', age: 22)
+  PassengerFlight.create!(flight_id: flight_1.id, passenger_id: passenger_1.id)
+  PassengerFlight.create!(flight_id: flight_1.id, passenger_id: passenger_2.id)
+
+  delta = Airline.create!(name: 'Delta')
+  flight_2 = delta.flights.create!(number: '2784', date: '01/11/21', departure_city: 'Hawai', arrival_city: 'New York')
+  passenger_3 = Passenger.create!(name: 'Luis', age: 37)
+  passenger_4 = Passenger.create!(name: 'Miguel', age: 33)
+  PassengerFlight.create!(flight_id: flight_2.id, passenger_id: passenger_3.id)
+  PassengerFlight.create!(flight_id: flight_2.id, passenger_id: passenger_4.id)
+
+  visit '/flights'
+
+    expect(page).to have_button("Remove #{passenger_1.name}")
+    click_button("Remove #{passenger_1.name}")
+    expect(current_path).to eq("/flights")
+    expect(page).to_not have_content(passenger_1.name)
+  end
 end
