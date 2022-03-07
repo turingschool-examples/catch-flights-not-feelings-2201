8a72954f4837b2d28d6c94d92cb5f17202190398
Jessica Grazulis <jlgrazulis@gmail.com>
2022-03-07 11:17:22 -0700

    Add destroy method

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,4 +3,10 @@ class FlightsController < ApplicationController
     # @flights = Flight.all
     @airlines = Airline.all
   end
+
+  def destroy
+    require 'pry'; binding.pry
+    # @flight = Flight.find(params{:id})
+    @flight.passengers.destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :airlines
   resources :flights do
     resources :passengers
   end
-    resources :airlines
+
 end
