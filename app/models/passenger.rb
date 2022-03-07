1685c664f6ea2b633c460e9a35c9d71066ec207e
Aedan Yturralde <aedanyturralde@gmail.com>
2022-03-07 11:44:56 -0700

    remove unneeded method, consolodated to airline model

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,7 +2,7 @@ class Passenger < ApplicationRecord
   has_many :flight_passengers
   has_many :flights, through: :flight_passengers
 
-  def self.return_adults
-    where('age > ?', 17)
-  end 
+  # def self.return_adults
+  #   where('age > ?', 17)
+  # end 
 end 
--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -7,18 +7,18 @@ RSpec.describe Passenger, type: :model do
   end 
 
   describe 'class methods' do 
-    describe '#return_adults' do 
-      it 'returns all passengers that are older than 17' do 
-        FlightPassenger.destroy_all
-        Passenger.destroy_all
-        passenger1 = Passenger.create!(name: "Tina", age: 15)
-        passenger2 = Passenger.create!(name: "Linda", age: 43)
-        passenger3 = Passenger.create!(name: "Louise", age: 12)
-        passenger4 = Passenger.create!(name: "Bob", age: 44)
+    # describe '#return_adults' do 
+    #   it 'returns all passengers that are older than 17' do 
+    #     FlightPassenger.destroy_all
+    #     Passenger.destroy_all
+    #     passenger1 = Passenger.create!(name: "Tina", age: 15)
+    #     passenger2 = Passenger.create!(name: "Linda", age: 43)
+    #     passenger3 = Passenger.create!(name: "Louise", age: 12)
+    #     passenger4 = Passenger.create!(name: "Bob", age: 44)
 
-        expect(Passenger.return_adults).to eq([passenger2, passenger4])
-      end 
-    end
+    #     expect(Passenger.return_adults).to eq([passenger2, passenger4])
+    #   end 
+    # end
     
   end 
 end
