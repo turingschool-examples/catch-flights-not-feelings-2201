b699da8fa788b1640abc0c6cd6ee55f216a521f3
Conor Barthel <conor.barthel@gmail.com>
2022-03-07 09:21:15 -0700

    Test: Relationship testing

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Flight, type: :model do
-  it { should belong_to :airline }
+  describe 'relationships' do
+    it {should have_many(:flight_passengers)}
+    it {should have_many(:passengers).through(:flight_passengers)}
+  end
 end
