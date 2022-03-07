a39a54fc323448c59d30da9b8ac87afe87e94ec2
Sam Iavri <sammi80134@gmail.com>
2022-03-07 09:39:07 -0700

    relationships set up

--- spec/models/passenger_flight_spec.rb
+++ spec/models/passenger_flight_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe PassengerFlight, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to :passenger }
+  it { should belong_to :flight }
 end
--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Passenger, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:passenger_flights) }
+  it { should have_many(:flights).through(:passenger_flights) }
 end
